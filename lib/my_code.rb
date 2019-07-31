be6aab4eca64a3e93696e6302be349515daf9da0
Kimberly Bone <k_bone@uncg.edu>
2019-07-31 02:42:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,10 +9,10 @@ def map(source_array)
 end 
 
 
-def reduce(source_array, starting_point = 0)
+def reduce(source_array, starting_point = nil)
   i = 0
     while i < source_array.length
-      starting_point = yield(starting_point, source_array[i])
+      starting_point = yield(starting_point, source_array[0])
       i += 1
     end
     starting_point

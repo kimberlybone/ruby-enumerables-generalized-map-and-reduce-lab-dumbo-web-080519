a180ebaaa66d184cf20e5ef87c179ed833597914
Kimberly Bone <k_bone@uncg.edu>
2019-07-31 02:46:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,7 @@ end
 
 
 def reduce(source_array, starting_point = nil)
-  i = 1
+  i = 0
     while i < source_array.length
       starting_point = yield(starting_point, source_array[i])
       starting_point = yield(starting_point, source_array[0])
